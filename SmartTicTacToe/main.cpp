e7a04035f24e7aa739739426e6c3ba43f155348b
TedXpro <johannes.woldeyes@gmail.com>
2026-04-18 18:37:29 +0300

    Modify UI to include player turns and best of 3 and best of 5

--- SmartTicTacToe/main.cpp
+++ SmartTicTacToe/main.cpp
@@ -26,6 +26,14 @@ vector<vector<char>> board = {
 char currentMarker; 
 int currentPlayer;
 
+int playerScore = 0;
+int computerScore = 0;
+int targetWins = 0;
+int bestOf = 0;
+int roundNumber = 0;
+char playerChoice;
+char computerChoice;
+
 int MinMax(int depth, bool isMax, char aiMarker, char playerMarker);
 bool IsMovesLeft();
 int EvaluateBoard(char aiMarker, char playerMarker);
@@ -33,81 +41,168 @@ bool CheckLine(int r1, int c1, int r2, int c2, int r3, int c3, char marker);
 bool FindBestMove(char markerToLookFor);
 void ComputerMove();
 void DrawBoard();
+void ResetBoard();
 bool CheckWin();
 string GetColoredMarker(char marker);
 
 int main() {
     srand(time(0));
-    
-    system("cls"); 
+    system("cls");
 
-    cout << CYAN << "Player 1 choose your Marker (X, O): " << RESET;
+    cout << YELLOW << "\n";
+    cout << "      ***********************" << endl;
+    cout << "      * TIC-TAC-TOE PRO   *" << endl;
+    cout << "      ***********************" << RESET << endl;
+    cout << "\n";
+
+    cout << CYAN << "  Choose your Marker (X or O): " << RESET;
     while (true) {
-        if (cin >> currentMarker) {
-            currentMarker = toupper(currentMarker);
-            if (currentMarker == 'X' || currentMarker == 'O') {
-                cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+        if (cin >> playerChoice) {
+            playerChoice = toupper(playerChoice);
+            if (playerChoice == 'X' || playerChoice == 'O') {
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 break;
             }
         } else {
-            cin.clear(); 
+            cin.clear();
         }
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
-        cout << RED << "Invalid Marker! Please choose X or O: " << RESET;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << RED << "  Invalid Marker! Please choose X or O: " << RESET;
     }
+    computerChoice = (playerChoice == 'X') ? 'O' : 'X';
 
-    currentPlayer = 1;
-    DrawBoard();
-
-    for (int i = 0; i < 9; i++) {
-        int slot;
-        if (currentPlayer == 1) {
-            bool validMove = false;
-            
-            do {
-                cout << "It's player " << currentPlayer << "'s turn (" << GetColoredMarker(currentMarker) << "). Enter slot: ";
-                
-                if (!(cin >> slot)) {
-                    cin.clear(); 
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
-                    cout << RED << "Invalid Input! Please enter a number 1-9." << RESET << endl;
-                    continue;
-                }
-
-                if (slot < 1 || slot > 9) {
-                    cout << RED << "Invalid Slot! Please choose 1-9." << RESET << endl;
-                    continue;
-                }
-                
-                int row = (slot - 1) / 3;
-                int col = (slot - 1) % 3;
-                
-                if (board[row][col] != 'X' && board[row][col] != 'O'){
-                    board[row][col] = currentMarker;
-                    validMove = true;
-                } else {
-                    cout << RED << "Slot Already Taken! Please choose another." << RESET << endl;
-                }
-            } while (!validMove);
+    int seriesChoice;
+    cout << CYAN << "  Choose series - Best of (3 or 5): " << RESET;
+    while (true) {
+        if (cin >> seriesChoice) {
+            if (seriesChoice == 3 || seriesChoice == 5) {
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                break;
+            }
         } else {
-            ComputerMove();
+            cin.clear();
         }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << RED << "  Invalid choice! Please enter 3 or 5: " << RESET;
+    }
+    bestOf = seriesChoice;
+    targetWins = (bestOf == 3) ? 2 : 3;
+
+    bool humanStartsNext = (rand() % 2 == 0);
+
+    while (playerScore < targetWins && computerScore < targetWins) {
+        roundNumber++;
+        ResetBoard();
+
+        if (humanStartsNext) {
+            currentPlayer = 1;
+            currentMarker = playerChoice;
+        } else {
+            currentPlayer = 2;
+            currentMarker = computerChoice;
+        }
+
         DrawBoard();
 
-        if (CheckWin()) {
-            cout << GREEN << "      ***********************" << endl;
-            cout << "      * PLAYER " << currentPlayer << " WINS!    *" << endl;
-            cout << "      ***********************" << RESET << endl;
-            return 0;
+        if (currentPlayer == 1) {
+            cout << CYAN << "  You go first!" << RESET << endl;
+        } else {
+            cout << CYAN << "  Computer goes first!" << RESET << endl;
         }
 
-        
-        currentPlayer = (currentPlayer == 1) ? 2 : 1;
-        currentMarker = (currentMarker == 'X') ? 'O' : 'X';
+        bool roundOver = false;
+        for (int i = 0; i < 9 && !roundOver; i++) {
+            if (currentPlayer == 1) {
+                bool validMove = false;
+                do {
+                    cout << "  Your turn (" << GetColoredMarker(playerChoice) << "). Enter slot: ";
+                    int slot;
+                    if (!(cin >> slot)) {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << RED << "  Invalid Input! Please enter a number 1-9." << RESET << endl;
+                        continue;
+                    }
+                    if (slot < 1 || slot > 9) {
+                        cout << RED << "  Invalid Slot! Please choose 1-9." << RESET << endl;
+                        continue;
+                    }
+                    int row = (slot - 1) / 3;
+                    int col = (slot - 1) % 3;
+                    if (board[row][col] != 'X' && board[row][col] != 'O') {
+                        board[row][col] = currentMarker;
+                        validMove = true;
+                    } else {
+                        cout << RED << "  Slot Already Taken! Choose another." << RESET << endl;
+                    }
+                } while (!validMove);
+            } else {
+                ComputerMove();
+            }
+
+            DrawBoard();
+
+            if (CheckWin()) {
+                if (currentPlayer == 1) {
+                    playerScore++;
+                    cout << GREEN << "      >> YOU WON ROUND " << roundNumber << "! <<" << RESET << endl;
+                } else {
+                    computerScore++;
+                    cout << RED << "      >> COMPUTER WON ROUND " << roundNumber << "! <<" << RESET << endl;
+                }
+                roundOver = true;
+            } else if (i == 8) {
+                cout << YELLOW << "      >> ROUND " << roundNumber << " IS A TIE! <<" << RESET << endl;
+            }
+
+            if (!roundOver) {
+                currentPlayer = (currentPlayer == 1) ? 2 : 1;
+                currentMarker = (currentMarker == 'X') ? 'O' : 'X';
+            }
+        }
+
+        cout << endl;
+        cout << CYAN << "      ----- Series Score -----" << RESET << endl;
+        cout << "      " << GREEN << "You: " << playerScore << RESET
+             << "  |  " << RED << "Computer: " << computerScore << RESET << endl;
+        cout << CYAN << "      -------------------------" << RESET << endl;
+
+        humanStartsNext = !humanStartsNext;
+
+        if (playerScore < targetWins && computerScore < targetWins) {
+            cout << endl << "  Press any key to continue to Round " << (roundNumber + 1) << "...";
+            system("pause > nul");
+        }
     }
 
-    cout << YELLOW << "It's a tie game!" << RESET << endl;
+    cout << endl;
+    if (playerScore >= targetWins) {
+        cout << GREEN;
+        cout << "      ============================" << endl;
+        cout << "        YOU WON THE SERIES!" << endl;
+        cout << "      ============================" << RESET << endl;
+    } else {
+        cout << RED;
+        cout << "      ============================" << endl;
+        cout << "        COMPUTER WINS THE SERIES!" << endl;
+        cout << "      ============================" << RESET << endl;
+    }
+    cout << endl;
+    cout << "      Final Score: " << GREEN << "You " << playerScore << RESET
+         << " - " << RED << computerScore << " Computer" << RESET << endl;
+    cout << endl;
+
     return 0;
+}
+
+void ResetBoard() {
+    int num = 1;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = '0' + num;
+            num++;
+        }
+    }
 }
 
 int MinMax(int depth, bool isMax, char aiMarker, char playerMarker) {
@@ -267,6 +362,13 @@ void DrawBoard() {
     cout << "      ***********************" << endl;
     cout << "      * TIC-TAC-TOE PRO   *" << endl;
     cout << "      ***********************" << RESET << endl;
+
+    // Round & score info
+    if (roundNumber > 0) {
+        cout << CYAN << "      Round " << roundNumber << " | Best of " << bestOf << RESET << endl;
+        cout << "      You (" << GetColoredMarker(playerChoice) << "): " << playerScore
+             << "  |  Computer (" << GetColoredMarker(computerChoice) << "): " << computerScore << endl;
+    }
     cout << "\n";
 
     cout << "           |     |     " << endl;
